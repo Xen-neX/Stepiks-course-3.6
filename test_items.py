232599f1356a6529241fa1ad71a1fc51d245b4a3
Xen <67798727+Xen-neX@users.noreply.github.com>
2021-06-09 14:01:56 +0300

    Update test_items.py
    Закомментирован time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_add_to_basket(browser):
     browser.get(link)
     # строку ниже нужно расскоментировать при проверке fr или другого языка!
-    time.sleep(30)
+    # time.sleep(30)
     assert browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"), 'Ошибка - кнопка не найдена'
